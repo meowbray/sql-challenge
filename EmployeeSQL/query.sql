c025ad8477ebd72e6a8be9dc5598c52e7d49b841
daniel mowbray <danielmowbray@mac.com>
2019-10-12 20:40:03 -0500

    mostly

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -3,12 +3,33 @@
 SELECT employee.emp_no, employee.last_name, employee.first_name, employee.gender, saleries.salary
 FROM employee
 RIGHT JOIN saleries ON employee.emp_no = saleries.emp_no
+
+--2
+SELECT * FROM
+employee WHERE hire_date = '1983'
+*/
+/*
+--3
+SELECT employee.emp_no, employee.last_name, employee.first_name, employee.hire_date, title.title
+FROM employee
+INNER JOIN title ON employee.emp_no = title.emp_no
+*/
+/*
+--4
+SELECT employee.emp_no, employee.last_name, employee.first_name, employee.hire_date, title.title
+FROM employee
+RIGHT JOIN title ON employee.emp_no = title.emp_no
+
+--5
+SELECT *
+FROM employee
+WHERE first_name='Hercules';
 */
 /*
 --7. List all employees in the Sales and Development departments,  employee number, last name, first name, and department name.
-SELECT employee.emp_no, employee.last_name, employee.first_name, dept_emp.dept_no
+SELECT employee.emp_no, employee.last_name, employee.first_name, title.title
 FROM employee
-RIGHT JOIN dept_emp ON employee.emp_no = dept_emp.emp_no
+RIGHT JOIN title ON employee.emp_no = title.emp_no
 */
 --8
 /*
@@ -18,3 +39,8 @@ SELECT last_name, COUNT(last_name)AS Frequency
 	ORDER BY
 	COUNT(last_name) DESC;
 */
+
+select * from employee
+WHERE first_name='Hercules'
+
+
